dd31d4858ca181cf7cca492bd501fcac4772c535
Maria J <cadking@gte.net>
2023-12-12 19:03:51 -0800

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent, NavbarComponent, HttpClientModule, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   title = 'onestopmedishop-admin';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,44 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { LoginComponent } from './components/admins/login/login.component';
+import { RegisterComponent } from './components/admins/register/register.component';
+import { AddmedicationComponent } from './components/medications/addmedication/addmedication.component';
+import { ManufacturersComponent } from './components/medications/manufacturers/manufacturers.component';
+import { MedicationsComponent } from './components/medications/medications.component';
+import { ViewmedicationComponent } from './components/medications/viewmedication/viewmedication.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { OrdersComponent } from './components/orders/orders.component';
+import { AdduserComponent } from './components/users/adduser/adduser.component';
+import { UsersComponent } from './components/users/users.component';
+import { ViewuserComponent } from './components/users/viewuser/viewuser.component';
+import { AuthGuards } from './guards/auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    { path :'' , component: DashboardComponent },
+    { path :'dashboard' , component: DashboardComponent },
+    { path :'orders' , component: OrdersComponent , canActivate: [AuthGuards]},
+
+    { path :'auth' , children :[
+        {path: 'login' , component: LoginComponent},
+        {path: 'register' , component: RegisterComponent},
+    ]},
+
+    { path :'users' , children :[
+        {path: '' , component: UsersComponent},
+        {path: 'create' , component: AdduserComponent},
+        {path: 'update' , component: AdduserComponent},
+        {path: 'view' , component: ViewuserComponent},
+    ], canActivate: [AuthGuards]},
+
+    { path :'medications' , children :[
+        {path: '' , component: MedicationsComponent},
+        {path: 'create' , component: AddmedicationComponent},
+        {path: 'update' , component: AddmedicationComponent},
+        {path: 'view' , component: ViewmedicationComponent},
+        {path: 'manufacturers' , component: ManufacturersComponent},
+    ], canActivate: [AuthGuards]},
+
+    { path:"**", component: NotFoundComponent}
+    
+];
