d7733fc9e3d2ef0dd1b751ad512b14c7d6b1246d
Maria J <cadking@gte.net>
2023-12-15 08:40:27 -0800

    corrected routes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-   apiBaseUrl :'ec2-18-188-94-121.us-east-2.compute.amazonaws.com:8083/medi'
-  // apiBaseUrl :'http://localhost:8081/medi'
+    apiBaseUrl :'ec2-18-188-94-121.us-east-2.compute.amazonaws.com:8083/medi'
+    // apiBaseUrl :'http://localhost:8083/medi'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,8 +5,8 @@
 
 export const environment = {
   production: false,
-  // apiBaseUrl :'http://ec2-3-23-94-142.us-east-2.compute.amazonaws.com:8081/ecomorg-webservice/api'
-  apiBaseUrl :'http://localhost:8081/medi'
+  apiBaseUrl :'ec2-18-188-94-121.us-east-2.compute.amazonaws.com:8083/medi'
+  // apiBaseUrl :'http://localhost:8083/medi'
 };
 
 /*
